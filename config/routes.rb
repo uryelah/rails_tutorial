d5e4158d7bf53187f7568d394b652fd885902337
sarah <sarachamorro@gmail.com>
2019-11-14 13:46:44 -0300

    finish chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   namespace :static_pages do
     get 'home'
-    get 'help', to: redirect('static_pages/contact')
-    get 'about'
-    get 'contact'
   end
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/help', to: 'static_pages#help', as: 'helf'
+  get '/signup', to: 'users#new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get signup_path
+    assert_response :success
+    assert_select "title", full_title("Sign up")
+  end
+end
