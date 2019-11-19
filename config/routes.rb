45ffe150fdaeb72a81f8e18128d372e6a27079ce
sarah <sarachamorro@gmail.com>
2019-11-19 17:12:44 -0300

    finish chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'sessions/new'
   namespace :static_pages do
     get 'home'
   end
@@ -9,6 +10,9 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help', as: 'helf'
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,30 +1,32 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
-  test "invalid signup information" do
+  test 'invalid signup information' do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
-                                         email: "user@invalid",
-                                         password:              "foo",
-                                         password_confirmation: "bar" } }
+      post users_path, params: { user: { name: '',
+                                         email: 'user@invalid',
+                                         password: 'foo',
+                                         password_confirmation: 'bar' } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert-danger'
   end
 
-    test "valid signup information" do
-      get signup_path
-      assert_difference 'User.count', 1 do
-        post users_path, params: { user: { name:  "Example User",
-                                           email: "user@example.com",
-                                           password:              "password",
-                                           password_confirmation: "password" } }
-      end
-      follow_redirect!
-      assert_template 'users/show'
-      assert_not flash.include?('danger') 
+  test 'valid signup information' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'Example User',
+                                         email: 'user@example.com',
+                                         password: 'password',
+                                         password_confirmation: 'password' } }
     end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.include?('danger')
+    assert is_logged_in?
+  end
 end
