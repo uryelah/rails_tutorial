ac83a358ceec1cdac684690c7a6831a3ce64a198
sarah <sarachamorro@gmail.com>
2019-11-21 17:37:08 -0300

    end chapter 12

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   namespace :static_pages do
     get 'home'
@@ -18,4 +20,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
